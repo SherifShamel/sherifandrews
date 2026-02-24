5fe860678c101ef8f231ea2ec1b28ee722416782
Sherif Shamel <sherifshamel99@gmail.com>
2026-02-24 10:34:04 +0200

    changes to align items in html file

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Experience Section
 async function getData() {
   let data = await fetch(
     "https://api.jsonbin.io/v3/b/6962520fd0ea881f40621007",
@@ -44,3 +45,107 @@ for (let index = 0; index < data.length; index++) {
                             </div>`;
   desc = [];
 }
+// End Of Experience Section
+
+// Projects Section
+let wordpressTab = document.querySelector("#wordpressContent");
+let wordpressContent = [];
+let nativeTab = document.querySelector("#native-tab");
+let frameworkTab = document.querySelector("#frameworkContent");
+
+let tags = [];
+let wordpressProjects = [];
+let frameworkProjects = [];
+
+// Wordpress projects
+async function getWordpressProjects() {
+  let data = await fetch(
+    "https://api.jsonbin.io/v3/b/697f2db2ae596e708f08e4ef",
+    {
+      headers: {
+        "X-Master-Key":
+          "$2a$10$qIHW.uBjT202MQY9ParQ3uy9HibyOY/o5K/J9ROIp1LXAsAeIL1TG",
+      },
+    },
+  ).then((e) => e.json());
+  return data.record;
+}
+wordpressProjects = await getWordpressProjects();
+console.log(wordpressProjects);
+
+for (let index = 0; index < wordpressProjects.length; index++) {
+  for (let tag = 0; tag < wordpressProjects[index].techtags.length; tag++) {
+    tags.push(wordpressProjects[index].techtags[tag]);
+  }
+  wordpressTab.innerHTML += `<div class="col-sm-12 col-lg-6">
+                                            <div class="card bg-transparent p-2 h-100">
+                                                <img src="${wordpressProjects[index].url}" class="card-img"
+                                                    alt="Quote Generator app">
+                                                <div class="card-body my-2">
+                                                    <h5 class="card-title main-text">${wordpressProjects[index].name}
+                                                    </h5>
+
+                                                   <p class="card-text text-white">${wordpressProjects[index].description}
+                                                   </p>
+                                                   <p>
+                                                       <a href="${wordpressProjects[index].demo}"
+                                                           target="_blank" class="main-text">View Demo</a>
+                                                   </p>
+                                                   <p class="card-text">tags: ${tags}</p>
+                                               </div>
+                                           </div>
+                                       </div>`;
+  tags = [];
+}
+
+// Framework Projects
+
+async function getFrameworkProjects() {
+  let data = await fetch(
+    "https://api.jsonbin.io/v3/b/697f5dfcae596e708f092f67",
+    {
+      headers: {
+        "X-Master-Key":
+          "$2a$10$qIHW.uBjT202MQY9ParQ3uy9HibyOY/o5K/J9ROIp1LXAsAeIL1TG",
+      },
+    },
+  ).then((e) => e.json());
+  return data.record;
+}
+frameworkProjects = await getFrameworkProjects();
+console.log(frameworkProjects);
+
+for (let index = 0; index < frameworkProjects.length; index++) {
+  for (let tag = 0; tag < frameworkProjects[index].techtags.length; tag++) {
+    tags.push(frameworkProjects[index].techtags[tag]);
+  }
+  frameworkTab.innerHTML += `<div class="col-sm-12 col-lg-6">
+                                            <div class="card bg-transparent p-2 rounded-4 h-100">
+                                              <div class="card-img-top">
+                                                <img class="w-100 rounded-3" src="${frameworkProjects[index].url}" class="card-img"
+                                                alt="${frameworkProjects[index].name}">
+                                              </div>    
+                                                <div class="card-body d-flex flex-column my-2">
+                                                    <h5 class="card-title main-text">${frameworkProjects[index].name}
+                                                    </h5>
+
+                                                   <p class="card-text text-white flex-grow-1">${frameworkProjects[index].description}
+                                                   </p>
+                                                   
+                                                   <div class="tags-section flex-grow-1">
+                                                    <p class="card-text mb-0 text-start fw-bold text-white">tags:</p>
+                                                      <ul class="tags-list">
+                                                      ${tags.map((tag) => `<li class="tag fw-bold text-white">${tag}</li>`).join("")}
+                                                      </ul>
+                                                   </div>
+                                                   <p>
+                                                       <a href="${frameworkProjects[index].demo}"
+                                                           target="_blank" class="demo-link">View Demo</a>
+                                                   </p>
+                                               </div>
+                                           </div>
+                                       </div>`;
+  tags = [];
+}
+
+// End OfProjects Section
